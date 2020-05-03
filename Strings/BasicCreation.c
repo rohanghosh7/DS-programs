dad7dd3fa38277e197bbb3efbf9051e33480afa0
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-04 01:14:04 +0530

    Add files via upload

---
+++ Strings/BasicCreation.c
@@ -0,0 +1,12 @@
+//Take a word as an input and print it
+
+#include<stdio.h>
+
+int main()
+{
+    char ch[100];
+    printf("Enter a name=");
+    scanf("%s",&ch);
+    printf("%s\n",ch);
+    return 0;
+}
