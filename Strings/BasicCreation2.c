36d646915f0c9938d923412ffa0166e0977f225b
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-05 23:11:40 +0530

    Add files via upload

---
+++ Strings/BasicCreation2.c
@@ -0,0 +1,12 @@
+//Take a sentence as an input and print it
+
+#include<stdio.h>
+
+int main()
+{
+    char ch[100];
+    printf("Enter a sentence=");
+    gets(ch);
+    printf("%s\n",ch);
+    return 0;
+}
