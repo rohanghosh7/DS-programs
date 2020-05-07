c77fb99be821594c0472fce9c54e4902e82a0a80
Rohan Ghosh <iamronnie40@gmail.com>
2020-05-08 01:27:20 +0530

    Add files via upload

---
+++ Strings/Length.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch[100];
+    printf("Enter a string");
+    scanf("%s",&ch);
+    int i;
+    for(i=0;ch[i]!='\0';i++)
+    {
+
+    }
+    printf("Length=%d",i);
+    return 0;
+}
