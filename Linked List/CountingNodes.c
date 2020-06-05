cacabeac8af469f9f274744d85d6ed2c93b75978
Rohan Ghosh <iamronnie40@gmail.com>
2020-06-06 02:35:43 +0530

    counting nodes

---
+++ Linked List/CountingNodes.c
@@ -0,0 +1,41 @@
+//Counting total number of nodes in a linked list
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    int i,n,count=0;
+    struct Node *head,*p,*t;
+    head=NULL;
+    printf("Enter number of elements to enter=");
+    scanf("%d",&n);
+    head=(struct Node *)malloc(sizeof(struct Node));
+    printf("Enter the first element=");
+    scanf("%d",&head->data);
+    head->next=NULL;
+    p=head;
+    printf("Enter rest all elements=");
+    for(i=1;i<n;i++)
+    {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        scanf("%d",&t->data);
+        t->next=NULL;
+        p->next=t;
+        p=p->next;
+    }
+    p=head;
+    while(p!=NULL)
+    {
+        count++;
+        p=p->next;
+    }
+    printf("Total number of nodes=%d",count);
+    return 0;
+}
